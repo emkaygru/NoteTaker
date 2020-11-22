77fe3105424849766dd157a2f07ba97c34670ce9
Emily Groenendal <emilylauder@gmail.com>
2020-11-22 17:37:22 -0500

    updating routes

--- routes/routes.js
+++ routes/routes.js
@@ -7,10 +7,43 @@ module.exports = app => {
 
         var notes = JSON.parse(data);
 
-        app.get('/api/notes', function (req, res) {
+        app.get('/api/notes', (req, res) => {
             res.json(notes);
-        })
+        });
 
+        app.post('/api/notes', (req, res) => {
+            let newNote = req.body;
+            notes.push(newNote);
+            fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+                if (err) throw err;
+                res.json(notes);
+            });
+
+        });
+
+        app.delete('/api/notes/:id', (req, res) => {
+            var id = req.params.id;
+
+            for (i = 0; i < notes.length; i++) {
+                if (id === notes[i].id) {
+                    notes.splice(i, 1);
+                };
+            };
+            fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+                if (err) throw err;
+                res.json(notes);
+            });
+        });
+
+        // ====== View Routes ======
+
+        app.get('/notes', (req, res) => {
+            res.sendFile(path.join(__dirname, '../public/notes.html'));
+        });
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, '../public/index.html'));
+        });
 
     });
 };
