7c059c8c5dadff8533df665151cc1f81a2248298
Emily Groenendal <emilylauder@gmail.com>
2020-11-23 19:11:40 -0500

    updated packages

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const notes = require("../db/db.json");
 
 module.exports = app => {
     fs.readFile('db/db.json', 'utf8', (err, data) => {
@@ -15,37 +17,39 @@ module.exports = app => {
         });
 
        
-        //retrieves note with specific id
-        app.get('/api/notes/:id', (req, res) => {
-            // displays json index for notes array of iD 
-            res.json(notes[req.params.id]);
-        });
-
+      
          // api post route 
          app.post('/api/notes', (req, res) => {
-            // new note variable 
+            req.body.id = uuidv4();
+           
             // receives a new notes, adds it to the db.json and returns a new note 
-            let newNote = req.body;
+            console.log(req.body);
             // notes pushed to notes array 
-            notes.push(newNote);
+            notes.push(req.body);
 
-            fs.writeFile('./db/db.json', JSON.stringify(notes,'/t'), (err) => {
+            fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
                 if (err) throw err;
                 res.json(notes);
             });
 
         });
-        
+
+          //retrieves note with specific id
+          app.get('/api/notes/:id', (req, res) => {
+            // displays json index for notes array of iD 
+            res.json(notes[req.params.id]);
+        });
+
         // deletes notes with specified id 
         app.delete('/api/notes/:id', (req, res) => {
             var id = req.params.id;
 
-            for (i = 0; i < notes.length; i++) {
-                if (id === notes[i].id) {
-                    notes.splice(i, 1);
-                };
-            };
-            fs.writeFile('./db/db.json', JSON.stringify(notes,'/t'), (err) => {
+            for(i=0;i < notes.length;i++) {
+                if(id === notes[i].id){
+                    notes.splice(i,1);
+                }
+            }
+            fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
                 if (err) throw err;
                 res.json(notes);
             });
--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 // const fs = require('fs');
 // const path = require('path');
 
+// app.get("/", function(req, res){
+//     res.send("Server is up and running.yoohoo big summer blowout");
+// })
+
 // initialize express
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // set up data parser 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
